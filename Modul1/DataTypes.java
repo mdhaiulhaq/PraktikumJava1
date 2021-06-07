a12450218c8664c54ab9c1960f7b7856bb883b3f
telurkucing <daiulm11@gmail.com>
2021-06-07 17:24:11 +0800

    Output dengan sout

--- Modul1/DataTypes.java
+++ Modul1/DataTypes.java
@@ -9,5 +9,14 @@ public class DataTypes{
 	double ipk = 3.99964512;
 	char nilaiAbjad = 'A';
 	boolean ganteng = true;
+
+	System.out.println("Nama depan : " + namaDepan);
+	System.out.println("Nama tengah : " + namaTengah);
+	System.out.println("Nama belakang : " + namaBelakang);
+	System.out.println("Usia : " + usia);
+	System.out.println("Target Kuliah : " + targetTahunKuliah);
+	System.out.println("IPK : " + ipk);
+	System.out.println("Nilai PBO : " + nilaiAbjad);
+	System.err.println("Ganteng : " + ganteng);
  }
 }
