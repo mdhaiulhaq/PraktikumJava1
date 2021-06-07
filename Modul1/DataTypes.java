6e4a68d69e34efbcc1201b55c73ef33f587419a9
telurkucing <daiulm11@gmail.com>
2021-06-07 17:41:35 +0800

    input dengan scanner

--- Modul1/DataTypes.java
+++ Modul1/DataTypes.java
@@ -1,5 +1,10 @@
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+
 public class DataTypes{
  public static void main(String[] args){
+
+	Scanner scanner = new Scanner(System.in);
 
 	String namaDepan = "Muhammad";
 	String namaTengah = "Dhaiul";
@@ -10,6 +15,19 @@ public class DataTypes{
 	char nilaiAbjad = 'A';
 	boolean ganteng = true;
 
+	System.out.print("Input Nama Depan : ");
+	namaDepan = scanner.nextLine();
+	System.out.print("Input Usia : ");
+	usia = scanner.nextInt();
+	System.out.print("Input IPK : ");
+	ipk = scanner.nextDouble();
+	System.out.print("");
+	System.out.print("Input Nilai Abjad : ");
+	nilaiAbjad = scanner.next().charAt(0);
+	System.out.print("Ganteng? : ");
+	ganteng = scanner.nextBoolean();
+	
+	System.out.println("========== OUTPUT ==========");
 	System.out.println("Nama depan : " + namaDepan);
 	System.out.println("Nama tengah : " + namaTengah);
 	System.out.println("Nama belakang : " + namaBelakang);
@@ -18,5 +36,7 @@ public class DataTypes{
 	System.out.println("IPK : " + ipk);
 	System.out.println("Nilai PBO : " + nilaiAbjad);
 	System.err.println("Ganteng : " + ganteng);
+
+	JOptionPane.showMessageDialog(null,"Hai, " + namaDepan + namaTengah + namaBelakang);
  }
 }
