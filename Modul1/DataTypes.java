2464cbcde77adc25487cbfeb048e88f1bdd7cc3c
telurkucing <daiulm11@gmail.com>
2021-06-07 17:18:06 +0800

    pindahin file

---
+++ Modul1/DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes{
+ public static void main(String[] args){
+
+	String namaDepan = "Muhammad";
+	String namaTengah = "Dhaiul";
+	String namaBelakang = "Haq";
+	int usia = 17;
+	int targetTahunKuliah = 2;
+	double ipk = 3.99964512;
+	char nilaiAbjad = 'A';
+	boolean ganteng = true;
+ }
+}
